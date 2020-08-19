1ee9c01d6e3f92769a4b90541a7e8ea320a9fbcc
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 22:27:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,9 @@ class CashRegister
   def apply_discount
     if discount
       self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
+      puts "Your new total is #{total}!"
+    else
+      "ERROR: No discount to apply."
     end
   end
 

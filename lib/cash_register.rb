00554664066bdf47c1564efafacb8b004eedc8f0
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 22:24:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :discount
 
-  @items = []
+  #@items = []
 
   def initialize(discount = nil)
     @total = 0
@@ -10,7 +10,6 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     actual_price = price * quantity
-    #@items << title
     @total = @total + actual_price
   end
 

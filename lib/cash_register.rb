95ea455bcb992ab6ad3b3309e7a993a53d70a3dd
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 22:23:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price)
+  def add_item(title, price, quantity = 1)
     @items << title
     @total = @total + price
   end

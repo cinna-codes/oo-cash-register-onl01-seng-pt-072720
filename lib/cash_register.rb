47d92aa58495bc920cb0e16198788f4d3bc3071d
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 00:37:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,16 +6,52 @@ class CashRegister
     @discount = discount
   end
 
+  # def add_item(title, price, quantity = 1)
+  #   actual_price = price * quantity
+  #   @total = @total + actual_price
+  #   @items ||= []
+  #   quantity.times do
+  #     @items << title
+  #   end
+  #   @items_with_prices ||= {}
+  #   @items_with_prices[title] = price
+  # end
+
+
   def add_item(title, price, quantity = 1)
-    actual_price = price * quantity
-    @total = @total + actual_price
-    @items ||= []
-    quantity.times do
-      @items << title
+      @price = price
+      @total = @total + price
+      @items ||= []
+      quantity.times do
+        @items << title
+      end
+
+      @last_quantity_bought = quantity
+      @last_price = price ## yes, we already have a reference to this as @price, but this is just
+                                     ## setting a new variable explicitly for our use case
     end
-    @items_with_prices ||= {}
-    @items_with_prices[title] = price
-  end
+
+    def void_last_transaction
+      ## now, down here, when we invoke this
+
+      total_last_transaction = @last_quantity_bought * @last_price
+
+      ## we can subtract the total of the last transaction.
+
+      @last_quantity_bought.times do
+        ## pop an item out
+      end
+    end
+
+
+
+
+
+
+
+
+
+
 
   def apply_discount
     if discount
@@ -30,15 +66,15 @@ class CashRegister
     @items
   end
 
-  def void_last_transaction
-    self.total = (self.total - (self.items_with_prices[self.items.last]))
-    items.pop
-    #return self.total
-    if items == []
-      self.total = 0.0
-      #return self.total
-    end
-    return self.total
-  end
+  # def void_last_transaction
+  #   self.total = (self.total - (self.items_with_prices[self.items.last]))
+  #   items.pop
+  #   #return self.total
+  #   if items == []
+  #     self.total = 0.0
+  #     #return self.total
+  #   end
+  #   return self.total
+  # end
 
 end
